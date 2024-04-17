57bec6b772efb5b431a7b3007123fe092b1b8a30
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-04-17 19:53:24 +0200

    .

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,5 +1,23 @@
+import ContactForm from '../../components/ContactForm/ContactForm';
+import SearchBox from '../../components/SearchBox/SearchBox';
+import ContactList from '../../components/ContactList/ContactList';
+import css from './ContactsPage.module.css';
+
 const ContactsPage = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <h1 className={css.mainTitle}>
+        Phone<span>book</span>
+      </h1>
+      <div className={css.phonebookWrapper}>
+        <div className={css.formAndFilterWrapper}>
+          <ContactForm />
+          <SearchBox />
+        </div>
+        <ContactList />
+      </div>
+    </div>
+  );
 };
 
 export default ContactsPage;

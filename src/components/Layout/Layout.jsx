024a4d362d95d66aef02cfc6b8bdaead1a8dc91c
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-04-21 12:06:43 +0200

    .

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import { Suspense } from 'react';
 import AppBar from '../AppBar/AppBar';
-// import css from './Layout.module.css';
+import css from './Layout.module.css';
 
 const Layout = ({ children }) => {
   return (
     <>
       <AppBar />
-      <Suspense fallback={null}>{children}</Suspense>
+      <Suspense fallback={<div className={css.suspens}></div>}>
+        {children}
+      </Suspense>
     </>
   );
 };

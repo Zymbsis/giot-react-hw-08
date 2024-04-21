a3eefe9163a95057c7ae576329f9d8a7cca1327d
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-04-21 23:00:04 +0200

    .

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../redux/auth/operations';
 import { selectUserName } from '../../redux/auth/slice';
 import css from './UserMenu.module.css';
+import { clearContacts } from '../../redux/contacts/slice';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
@@ -10,7 +11,13 @@ const UserMenu = () => {
   return (
     <div className={css.userMenu}>
       <p>{`Welcome, ${userName}`}</p>
-      <button type="button" onClick={() => dispatch(logout())}>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(logout());
+          dispatch(clearContacts());
+        }}
+      >
         LogOut
       </button>
     </div>

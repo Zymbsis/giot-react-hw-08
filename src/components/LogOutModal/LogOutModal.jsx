3956243fe993d10ed6169260bc90cdb60b53719d
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-04-22 16:47:15 +0200

    .

--- src/components/LogOutModal/LogOutModal.jsx
+++ src/components/LogOutModal/LogOutModal.jsx
@@ -1,18 +1,18 @@
 import { useDispatch } from 'react-redux';
 import { logout } from '../../redux/auth/operations';
 import css from './LogOutModal.module.css';
+import { modalClose } from '../../redux/modal/slice';
 
 const LogOutModal = () => {
   const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(logout('LogOutModal'));
+    dispatch(modalClose());
+  };
   return (
     <div className={css.logoutModal}>
       <p>Are you sure you want to log out?</p>
-      <button
-        type="button"
-        onClick={() => {
-          dispatch(logout('LogOutModal'));
-        }}
-      >
+      <button type="button" onClick={handleClick}>
         LogOut
       </button>
     </div>

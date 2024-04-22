19eaa42dd6edd1bf8016d28e3af2fce73d95a3e4
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-04-22 21:43:17 +0200

    .

--- src/components/DeleteContactModal/DeleteContact.jsx
+++ src/components/DeleteContactModal/DeleteContact.jsx
@@ -1,22 +1,30 @@
 import { useDispatch, useSelector } from 'react-redux';
 import css from './DeleteContact.module.css';
 import { deleteContact } from '../../redux/contacts/operations';
-import { selectActionData } from '../../redux/modal/slice';
+import { modalClose, selectActionData } from '../../redux/modal/slice';
 
 const DeleteContact = () => {
   const dispatch = useDispatch();
   const contact = useSelector(selectActionData);
-  const handleClick = () => {
+  const handleDelete = () => {
     dispatch(deleteContact(contact.id));
+  };
+  const handleCancel = () => {
+    dispatch(modalClose());
   };
 
   return (
     <div className={css.deleteModal}>
       <p>Are you sure you want to delete this contact?</p>
       <p>{contact.name}</p>
-      <button type="button" onClick={handleClick}>
-        LogOut
-      </button>
+      <div className={css.buttonWrapper}>
+        <button type="button" onClick={handleDelete}>
+          Delete
+        </button>
+        <button type="button" onClick={handleCancel}>
+          Cancel
+        </button>
+      </div>
     </div>
   );
 };

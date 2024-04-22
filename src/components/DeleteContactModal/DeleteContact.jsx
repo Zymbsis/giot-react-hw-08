1adad28ff845d5434dda7f3319dd6d2c116e87ee
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-04-22 23:14:36 +0200

    .

--- src/components/DeleteContactModal/DeleteContact.jsx
+++ src/components/DeleteContactModal/DeleteContact.jsx
@@ -1,11 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from '../../redux/contacts/operations';
+import { selectLoading } from '../../redux/contacts/slice';
 import { modalClose, selectActionData } from '../../redux/modal/slice';
+
+import Loader from '../Loader/Loader';
+
 import css from './DeleteContact.module.css';
 
 const DeleteContact = () => {
   const dispatch = useDispatch();
   const contact = useSelector(selectActionData);
+  const contactLoading = useSelector(selectLoading);
   const handleDelete = () => {
     dispatch(deleteContact(contact.id));
   };
@@ -14,18 +19,24 @@ const DeleteContact = () => {
   };
 
   return (
-    <div className={css.deleteModal}>
-      <p>Are you sure you want to delete this contact?</p>
-      <p>{contact.name}</p>
-      <div className={css.buttonWrapper}>
-        <button type="button" onClick={handleDelete}>
-          Delete
-        </button>
-        <button type="button" onClick={handleCancel}>
-          Cancel
-        </button>
-      </div>
-    </div>
+    <>
+      {contactLoading ? (
+        <Loader />
+      ) : (
+        <div className={css.deleteModal}>
+          <p>Are you sure you want to delete this contact?</p>
+          <p>{contact.name}</p>
+          <div className={css.buttonWrapper}>
+            <button type="button" onClick={handleDelete}>
+              Delete
+            </button>
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 

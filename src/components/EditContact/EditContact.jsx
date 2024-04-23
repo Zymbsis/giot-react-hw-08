58f8219cf5f8537a537ef59bbae362556286e789
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-04-23 15:24:16 +0200

    .

--- src/components/EditContact/EditContact.jsx
+++ src/components/EditContact/EditContact.jsx
@@ -3,54 +3,65 @@ import { modalClose } from '../../redux/modal/slice';
 import css from './EditContact.module.css';
 import { updateContact } from '../../redux/contacts/operations';
 import { selectContact } from '../../redux/contacts/slice';
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import CreateInput from '../CreateInput/CreateInput';
 
 const EditContact = () => {
   const dispatch = useDispatch();
   const contact = useSelector(selectContact);
-  const credentials = { name: contact.name, number: contact.number };
-  const handleEdit = () => {
+
+  const handleSubmit = (values, action) => {
     dispatch(
       updateContact({
         id: contact.id,
-        credentials: credentials,
+        values,
       })
     );
+    action.resetForm();
   };
 
   const handleCancel = () => {
     dispatch(modalClose());
   };
-  const handleChangeName = e => {
-    credentials.name = e.target.value;
-    console.log(credentials);
-  };
-  const handleChangeNumber = e => {
-    credentials.number = e.target.value;
-  };
+  const FeedbackSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, 'Too Short! Length must be between 3 and 50 characters')
+      .max(50, 'Too Long! Length must be between 3 and 50 characters')
+      .required('Required'),
+    number: Yup.string()
+      .min(3, 'Too Short! Length must be between 3 and 50 characters')
+      .max(50, 'Too Long! Length must be between 3 and 50 characters')
+      .required('Required'),
+  });
   return (
     <div className={css.editModal}>
-      <div className={css.searchForm}>
-        <input
-          type="name"
-          name="name"
-          value={credentials.name}
-          onChange={handleChangeName}
-        />
-        <input
-          type="text"
-          name="number"
-          value={credentials.number}
-          onChange={handleChangeNumber}
-        />
-      </div>
-      <div className={css.buttonWrapper}>
-        <button type="button" onClick={handleEdit}>
-          Edit
-        </button>
-        <button type="button" onClick={handleCancel}>
-          Cancel
-        </button>
-      </div>
+      <Formik
+        initialValues={{ name: contact.name, number: contact.number }}
+        onSubmit={handleSubmit}
+        validationSchema={FeedbackSchema}
+      >
+        <Form className={css.form}>
+          <CreateInput
+            name="name"
+            type="text"
+            wrapperClassName={css.inputWrapper}
+            invalidClassName={css.invalid}
+          />
+          <CreateInput
+            name="number"
+            type="tel"
+            wrapperClassName={css.inputWrapper}
+            invalidClassName={css.invalid}
+          />
+          <div className={css.buttonWrapper}>
+            <button type="submit">Edit</button>
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
+        </Form>
+      </Formik>
     </div>
   );
 };
